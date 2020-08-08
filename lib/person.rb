adeef09179391a077f0eb88b1a8ec336a97db458
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-08 00:59:46 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+  :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width
   #your code here
+  def initialize(hash)
+    hash.each {|key, value| self.send(("#{key}="),value)}
+  end
 end
